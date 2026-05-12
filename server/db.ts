605730499bc0c35ff40642ed59d31bb73bed5369
Antony <your-email@gmail.com>
2026-05-12 17:00:04 +0300

    Fix db schema registration and add SSL

--- server/db.ts
+++ server/db.ts
@@ -1,6 +1,10 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "../shared/schema";
+import { users, payments, subscriptions } from "../shared/schema";
 
-const client = postgres(process.env.DATABASE_URL!);
-export const db = drizzle(client, { schema });
+const client = postgres(process.env.DATABASE_URL!, {
+  ssl: "require",
+});
+
+export const db = drizzle(client, { schema: { users, payments, subscriptions } });
