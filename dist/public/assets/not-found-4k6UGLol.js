d204360a43d196a42551184480c2228b50855c28
kstingjunior-svg <mulaanthonyke@gmail.com>
2026-07-03 16:51:01 +0300

    Fix: free services on /services page now route to their /tools/* page instead of /api/pay

--- dist/public/assets/not-found-4k6UGLol.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./react-vendor-r13h66Nr.js";import{O as r}from"./index-BcqykYm4.js";import"./router-vendor-Dslbjqcu.js";import"./vendor-BYT3tqSa.js";import"./query-vendor-BU5kfurC.js";import"./ui-vendor-DUnGUYrH.js";import"./i18n-vendor-DM6u5B64.js";import"./firebase-vendor-CrEDS3kv.js";function a(){return o.jsx(r,{type:"notfound",code:"404"})}export{a as default};
